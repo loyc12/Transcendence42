da91dae3be86751049c1a35d2fc128e4e9f0379f
Loïc <lordloyc@gmail.com>
2023-10-13 16:57:48 +0000

    minor (gitquick)

--- ShellScripts/.cmds.sh
+++ ShellScripts/.cmds.sh
@@ -9,10 +9,10 @@ alias GU='echo "\n > updating remote repos  < \n" &! git remote update' #							
 alias GA='echo "\n > adding all changes to commit queue \033[1;91m!!! < \n" &! git add * || git add -u' #	git add
 alias GC='echo "\n > commiting change queue with given message < \n" &! git commit -m' #					git commit
 alias GG='echo "\n > switching to given branch < \n" &! git switch' #										git go
-alias GNB='echo "\n > ! creating new specified branch ! < \n" &! git push origin -u' #						git go
+alias GNB='echo "\n > ! creating new specified branch ! < \n" &! git push origin -u' #						git push-on-new-branch
 alias GMRG='echo "\n > !! merging from given branch !! < \n" &! git merge' #								git merge
 alias GPSH='echo "\n > !!! pushing commited changes to branch !!! < \n" &! git push' #						git push
-alias GCQP='echo "\n > !!! making quick commit !!! < \n" &! $HOME/.gitquick.zsh' #							git quickpush
+alias GQCKP='echo "\n > !!!! making quick commit !!!! < \n" &! $SCRIPTS/.gitquick.sh' #						git quickpush
 
 
 # one for fetch
