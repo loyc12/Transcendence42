5d9f93f97db283353ca8f36f5f004b3966045c2c
alvachon <alvachon@student.42quebec.com>
2023-10-13 16:20:49 +0000

    docker ok

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    //return 'WOWOW World!';
+    return `<h1>Wow World Incroyable !</h1>`
   }
 }
