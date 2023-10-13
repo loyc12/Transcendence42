4767bdfc49dd587c24be7fdcb3cf59cbfe114fe9
Loïc <lordloyc@gmail.com>
2023-10-13 16:32:13 +0000

    minor

--- ShellScripts/.cmds.sh
+++ ShellScripts/.cmds.sh
@@ -2,14 +2,19 @@
 source $SCRIPTS/.help.sh
 
 # GIT COMMANDS
-alias gg='echo "\n\033[1;96mcloning repo from given address\033[0m\n" &! git clone'
-alias gs='echo "\n\033[1;92mchecking branch status\033[0m\n" &! git status'
-alias gu='echo "\n\033[1;92mpulling branch updates\033[0m\n" &! git pull'
-alias gr='echo "\n\033[1;92mupdating remote repos \033[0m\n" &! git remote update'
-alias ga='echo "\n\033[1;93madding all changes to commit queue \033[1;91m!!!\033[0m\n" &! git add * &! git add -u'
-alias gc='echo "\n\033[1;93mcommiting change queue with given message\033[0m\n" &! git commit -m'
-# alias gp='echo "\n\033[1;93mpushing commited changes to branch \033[1;91m!!!\033[0m\n" &! git push'
-# alias gq='echo "\n\033[1;93mmaking quick commit \033[1;91m!!!\033[0m\n" &! $HOME/.gitquick.zsh'
+alias GO='echo "\n > cloning repo from given address < \n" &! git clone' #									git optain
+alias GS='echo "\n > checking branch status < \n" &! git status' #											git status
+alias GP='echo "\n > pulling branch updates < \n" &! git pull' #											git pull
+alias GU='echo "\n > updating remote repos  < \n" &! git remote update' #									git update (remote)
+alias GA='echo "\n > adding all changes to commit queue \033[1;91m!!! < \n" &! git add * || git add -u' #	git add
+alias GC='echo "\n > commiting change queue with given message < \n" &! git commit -m' #					git commit
+alias GG='echo "\n > switching to given branch < \n" &! git switch' #										git go
+alias GM='echo "\n > !! merging from given branch !! < \n" &! git merge' #									git merge
+alias GG='echo "\n > ! creating new specified branch ! < \n" &! git push origin -u' #										git go
+
+# alias gpush='echo "\n > !!! pushing commited changes to branch !!! < \n" &! git push'						git push
+# alias gqckp='echo "\n > !! making quick commit !! < \n" &! $HOME/.gitquick.zsh' #							git quickpush
+
 # one for fetch
 # one for merge from DEV
 # one for merge from STABLE
@@ -17,8 +22,8 @@ alias gc='echo "\n\033[1;93mcommiting change queue with given message\033[0m\n" 
 # use script to fix order issues (use gq script as example)
 
 # ENV COMMANDS
-alias BUILD="docker build -t project-transcendence ."
-alias MAKE="docker run -it --name project-transcendence project-transcendence"
-alias STOP="docker stop project-transcendence"
-alias START="docker start -i project-transcendence"
-alias DELETE="docker rm -f project-transcendence"
+alias CONT_BUILD="docker build -t project-transcendence ."
+alias CONT_MAKE="docker run -it --name project-transcendence project-transcendence"
+alias CONT_STOP="docker stop project-transcendence"
+alias CONT_START="docker start -i project-transcendence"
+alias CONT_DELETE="docker rm -f project-transcendence"
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 
+# HOW TO USE : run 'source start.sh' in the Transcendence42 repository's terminal
+
 # SHORTCUTS
 export REPO="/workspaces/Transcendence42"
 export SCRIPTS="/workspaces/Transcendence42/ShellScripts"
