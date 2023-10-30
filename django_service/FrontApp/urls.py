6560927c72ac183e06ff89f9ceb5d2901c135205
alvachon <alvachon@student.42quebec.com>
2023-10-30 17:07:33 +0000

    not working waiting for Germ version

--- django_service/FrontApp/urls.py
+++ django_service/FrontApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_generic_hello),
-    path('hello/', views.say_hello_to_my_little_puppy),
-    path('list/', views.list_members)
+    # path('', views.say_generic_hello),
+    # path('hello/', views.say_hello_to_my_little_puppy),
+    # path('list/', views.list_members)
 ]
