8fa1fab8afc4e5f376495bfb47e678cbd89ea898
Ian Antony <57071620+iAntopy@users.noreply.github.com>
2023-10-30 18:02:28 +0000

    added static files finders in core/settings.py

--- django_service/core/settings.py
+++ django_service/core/settings.py
@@ -146,6 +146,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
