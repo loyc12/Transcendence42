3ab4411ff3e67711c630e1a578b1323ca91f6bc2
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-11-03 23:47:35 +0000

    updated

--- webpage/core/settings.py
+++ webpage/core/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     "Home",
     "frontHome", # TODO : DELETE AFTER MIGRATION
 ]
