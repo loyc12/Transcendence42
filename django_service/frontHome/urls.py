3e62d2e8859d09c64fdc3489094dd0077273778a
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-11-03 02:28:23 +0000

    rendu a tester arborescence dossier

--- django_service/frontHome/urls.py
+++ django_service/frontHome/urls.py
@@ -8,5 +8,5 @@ from . import views
 # ]
 
 urlpatterns = [
-    path('', views.jai_faim)
+    path('', views.index)
 ]
--- django_service/frontHome/views.py
+++ django_service/frontHome/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from django.template import loader
+
+# from articles.models import Article
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+    
+
+    #     return HttpResponse
+    #     return render(request, 'master/master.html')
+# def index(request):
+#     template = loader.get_template('index.html')
+#     # return HttpResponse(template.render())
+#     # return render(template.render())
+#     return render(template.render())
 
 # Create your views here.
-def jai_faim(request):
-    return HttpResponse('WOW')
+# def master(request):
+#     return render(request, 'master/master.html')
