22504bc4870e6a72c1f7f4614438b0d4d0974db7
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2023-11-06 11:35:16 -0500

    django + postgres run on wsl2. make has to be run in a powershell window. Not in VS Code terminal but its own window.

--- django_service/entrypoint.sh
+++ django_service/entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-while ! pg_isready -d djangodb -h '192.130.0.2' -p 5432 -U postgres; do
-#while ! psql -d djangodb -h '192.130.0.2' -p 5432 -U postgres < $DB_PASS; do
-    echo "Waiting for postgres connection ..." >> db_conn.log
-    sleep 1
-done
-echo 'SUCCESSFULLY CONNECTED TO POSTGRES FROM DJANGO !!' >> db_conn.log
-
-exec $@
+while ! pg_isready -d djangodb -h '192.130.0.2' -p 5432 -U postgres; do echo "Waiting for postgres connection ..." >> db_conn.log && sleep 1; done; echo 'SUCCESSFULLY CONNECTED TO POSTGRES FROM DJANGO !!' >> db_conn.log; exec $@;
