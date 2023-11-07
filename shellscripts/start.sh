519197b9b07512a7116f493ce2acf491cd4f7286
Loïc <lordloyc@gmail.com>
2023-11-07 15:58:14 +0000

    confirmed this version work

--- shellscripts/start.sh
+++ shellscripts/start.sh
@@ -1,5 +1,6 @@
 
 # HOW TO USE : run 'source start.sh' in the Transcendence42 repository's terminal
+# Alternatively : you can follow guide.txt to have your terminal automatically run this script on startup
 
 # SHORTCUTS
 export REPO="/workspaces/Transcendence42"
