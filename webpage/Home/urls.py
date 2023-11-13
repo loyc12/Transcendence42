10e6b772a6549245279b17a465ed6d784919d85e
alvachon <alvachon@student.42quebec.com>
2023-11-13 20:24:26 +0000

    post request written for token but buggy atm

--- webpage/Home/urls.py
+++ webpage/Home/urls.py
@@ -1,11 +1,11 @@
 """ This file contains the URL patterns for the Home app. """
 from django.urls import path
-from .views import home_view, login_view, api_view
+from .views import home_view, login_view, api_view#, get_token
 
 urlpatterns = [
     # URL for the main page (home)
     path("", home_view),
     path('index/', login_view, name='index'),
     path('login/', api_view, name='42login'),
-    # path('api/', api_login, name='api'),
+    #path('api/', get_token, name='api'),
 ]
--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -1,7 +1,9 @@
 """ This file is used to render the home page and login page. """
-#import json
+#from django.http import HttpResponse
 from core.settings import ENV_FILE
 from django.shortcuts import render, redirect
+#import requests
+
 
 def login_view(request):
     return render(request, 'Index/index.html')
@@ -27,9 +29,43 @@ def api_view(request):
                 
 #   Redirecting to the api call, put the callback url in a variable
     callback = redirect(api_url)
-    
 #  Return to the callback url, contain a code to get the token
     return (callback)
+    #return (get_token(callback))
+
+
+# POST request to get the token
+# def get_token(request):
+#     # Get the code from the callback url
+#     code = request.GET.get('code')
+    
+# #   Making the url for the api call
+# #   Manque pt code_verifier = https://www.oauth.com/oauth2-servers/access-tokens/authorization-code-request/
+#     api_url =   ENV_FILE['HTTP_PROTOCOL'] + \
+#                 ENV_FILE['APP42_DOMAIN'] + \
+#                 ENV_FILE['APP42_TOKEN'] + \
+#                 '?grant_type=authorization_code' + \
+#                 '&code=' + code + \
+#                 '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT'] + \
+#                 '&client_id=' + ENV_FILE['APP42_UID'] + \
+#                 '&client_secret=' + ENV_FILE['APP42_SECRET']
+    
+# #   POST request to get the token
+#     r = requests.post(api_url)
+#     return (HttpResponse(r.text))
+#     # access_token = r.json()['access_token']
+#     # expires_in = r.json()['expires_in']
+#     # if (r.json()['error']):
+#     #     endpoint = HttpResponse(".Error from the API.")
+#     # else:
+#     #     endpoint = HttpResponse(access_token + " " + expires_in)
+#     # return (endpoint)
+    
+    
+    
+    
+    
+
 
 #Return Code
 #http://127.0.0.1:3000/?code=8f05c9296f67d4b1cab461944c0c627672ed12f667d9100c745c9514b991521b
