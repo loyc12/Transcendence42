644d72d75034afec6c05464dd65872a7432b5ca6
alvachon <alvachon@student.42quebec.com>
2023-11-13 17:45:23 +0000

    Enquete sur le redirect uri dans 42api pour bazinga est le meme que celui dans le code. 42API retourne un erreur : The redirect uri included is not valid.

--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -2,10 +2,11 @@
 import json
 from core.settings import ENV_FILE
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect, HttpResponseRedirect#, HttpResponsePermanentRedirect
-from django.urls import reverse
+from django.shortcuts import render, redirect#, HttpResponseRedirect#, HttpResponsePermanentRedirect
+from django.urls import reverse_lazy#, reverse
 #from urllib.parse import urlencode, urlunsplit, quote_plus
 #import requests
+#from django.contrib.auth.decorators import permission_required
 
 def login_view(request):
     return render(request, 'Index/index.html')
@@ -16,17 +17,26 @@ def home_view(request):
         context['show_login_form'] = True
     return render(request, 'Home/home.html', context)
 
+#@permission_required('is_superuser')
 def api_view(request):
-    context = {"session": request.session.get("user"),
-               "pretty": json.dumps(request.session.get("user"), indent=4),}
-    redirect('https://api.intra.42.fr/oauth/authorize?client_id='+ ENV_FILE['APP42_UID'] + '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT'] + '&response_type=code')
-    return (HttpResponseRedirect(login_view))
+    # Overkill for now, suppose to be the user id code part.
+    # context = {"session": request.session.get("user"),
+    #            "pretty": json.dumps(request.session.get("user"), indent=4),}
+    
+    api_url =   ENV_FILE['HTTP_PROTOCOL'] + \
+                ENV_FILE['APP42_DOMAIN'] + \
+                ENV_FILE['APP42_AUTH'] + \
+                '?client_id=' + ENV_FILE['APP42_UID'] + \
+                '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT'] + \
+                '&response_type=code'
+    callback = redirect(api_url)
+    print(callback)
+    return (callback)
+    #return (reverse_lazy(login_view))
+    
+#https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-060054ee82cdef75be259160866ffaa26f98cef72e59311abfcb9bc609175caf&redirect_uri=http%3A%2F%2F2127.0.0.1%3A3000&response_type=code
 
-#   &redirect_uri=23.96.3.23:3000/oauth/receive_code
-
-#redirect_uri=23.96.3.23:3000&response_type=code
-#&redirect_uri=23.96.3.23:3000/index&response_type=code
-
+# https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-060054ee82cdef75be259160866ffaa26f98cef72e59311abfcb9bc609175caf&redirect_uri=http%3A%2F%2F2127.0.0.1%3A3000%2&response_type=code
 # https://docs.djangoproject.com/en/4.2/topics/auth/default/
 #keyword :token_generator
 # @decorator blabla token
