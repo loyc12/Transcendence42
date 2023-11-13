1fb6948e1b4556acfda2268bce7e771320d98b19
alvachon <alvachon@student.42quebec.com>
2023-11-13 14:43:53 +0000

    get to the api page, redirect uri is not the good one

--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -1,17 +1,14 @@
 """ This file is used to render the home page and login page. """
 import json
-#import django.shortcuts
 from core.settings import ENV_FILE
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect#, #HttpResponse, HttpResponsePermanentRedirect
-from django.urls import reverse
+#from django.urls import reverse
 #from urllib.parse import urlencode, urlunsplit, quote_plus
 #import requests
 
-
 def login_view(request):
     return render(request, 'Index/index.html')
-
 
 def home_view(request):
     context = {}
@@ -22,4 +19,12 @@ def home_view(request):
 def api_view(request):
     context = {"session": request.session.get("user"),
                "pretty": json.dumps(request.session.get("user"), indent=4),}
-    return redirect('https://api.intra.42.fr/oauth/authorize?client_id=' + ENV_FILE['APP42_UID'] + '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT'] + '&response_type=code')
+    return redirect('https://api.intra.42.fr/oauth/authorize?client_id='+ ENV_FILE['APP42_UID'] + '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT'] + '&response_type=code')
+
+#   &redirect_uri=23.96.3.23:3000/oauth/receive_code
+
+# https://docs.djangoproject.com/en/4.2/topics/auth/default/
+#keyword :token_generator
+# @decorator blabla token
+# def api_callback(request):
+#     return render(request, 'Home/home.html')
