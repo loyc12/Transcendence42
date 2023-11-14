fccad234655a13b4037d6f05b03ce55330a8acbf
alvachon <alvachon@student.42quebec.com>
2023-11-14 20:30:25 +0000

    oauth done, next step : clean code, putting retrieved data to database, creating a mode logged, creating a model for the network (?)

--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -2,9 +2,7 @@ import requests
 from django.http import HttpResponseRedirect, HttpResponse
 from core.settings import ENV_FILE
 from django.shortcuts import render
-#import urllib.parse, urllib.request
 
-#1352efde99192194de7410129bc9f05c943a78e15490defba18aa95aaa947ed5"
 
 def login_view(request):
     return render(request, 'Index/index.html')
@@ -43,16 +41,25 @@ def get_access_token(autorization_code):
                 
     client_id     = ENV_FILE['APP42_UID']
     client_secret = ENV_FILE['APP42_SECRET']
-    #redirect_uri  = ENV_FILE['APP42_OAUTH_REDIRECT']
     
-    data =  '?grant_type=client_credentials' + \
+    data =  '?grant_type=authorization_code' + \
             '&client_id=' + client_id + \
             '&client_secret=' + client_secret + \
-            '&code=' + autorization_code
+            '&code=' + autorization_code + \
+            '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT']
     return (url + data)
     
 def get_data():
     url = ENV_FILE['HTTP_PROTOCOL'] + \
           ENV_FILE['APP42_DOMAIN'] + \
-          "/v2/users/"
+          "/v2/me/"
     return (url)
+
+# "login":"alvachon",
+# "displayname":"Alexandra Vachon"
+
+# "image":{"link":"https://cdn.intra.42.fr/users/1a6cf4672ded9069b92b9e7d9a39acb0/alvachon.jpg",
+# "versions":{"large":"https://cdn.intra.42.fr/users/c26d9889073d41bdcf67cb0043ed2b6a/large_alvachon.jpg",
+# "medium":"https://cdn.intra.42.fr/users/11f8cb615ee7b6d7494be87753c105da/medium_alvachon.jpg",
+# "small":"https://cdn.intra.42.fr/users/910efb53059ce3da462a6b86d0b93034/small_alvachon.jpg",
+# "micro":"https://cdn.intra.42.fr/users/c36d792fe5c1287dd62b58f15ca4908f/micro_alvachon.jpg"}},
