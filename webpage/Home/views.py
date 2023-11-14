6891903d43b1f5227a06270ae164702e44ceb856
alvachon <alvachon@student.42quebec.com>
2023-11-14 19:01:28 +0000

    Able to retrieve all info of EVERYONE AHAHAHAH

--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -4,7 +4,7 @@ from core.settings import ENV_FILE
 from django.shortcuts import render
 #import urllib.parse, urllib.request
 
-
+#1352efde99192194de7410129bc9f05c943a78e15490defba18aa95aaa947ed5"
 
 def login_view(request):
     return render(request, 'Index/index.html')
@@ -13,8 +13,14 @@ def home_view(request):
     authorization_code = request.GET.get('code', None)
     if (authorization_code):
         token = get_access_token(authorization_code)
-        access_token = requests.post(token)
-        return (HttpResponse(access_token))
+        token_code = requests.post(token)
+        
+        access_token = token_code.json()['access_token']
+        
+        headers = {'Authorization': 'Bearer ' + access_token}
+        url = get_data()
+        user_data = requests.get(url, headers=headers)
+        return (HttpResponse(user_data.text))
     return render(request, 'Home/home.html')
 
 def api_view(request):
@@ -45,6 +51,8 @@ def get_access_token(autorization_code):
             '&code=' + autorization_code
     return (url + data)
     
-    # with urllib.request.urlopen(url, data=data) as response:
-    #     token_response = response.read()
-    #     return(token_response)
+def get_data():
+    url = ENV_FILE['HTTP_PROTOCOL'] + \
+          ENV_FILE['APP42_DOMAIN'] + \
+          "/v2/users/"
+    return (url)
