02500d077f1f0b8273b294acefa8d0cf742487fe
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-11-15 18:41:37 +0000

    not much of improve

--- webpage/Home/static/js/script.js
+++ webpage/Home/static/js/script.js
@@ -1,78 +1,138 @@
-// vanilla-script.js
-console.log("Script loaded successfully!");
+//  Initialize the current state to 'home'
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    var module1Content = '<div>Content for Module 1</div>';
-    var module2Content = '<div>Content for Module 2</div>';
-    var listContent = '<div>Content for ListAlt</div>';
-    var HeroDiv = document.querySelector('.HeroDiv');
-    var NavBar = document.querySelector('.NavBar');
+let currentState = 'home';  // Initialize the current state to 'home'
+let initialChoice = null;    // Keep track of the initial choice
 
-    // Add click event listeners to NavBar links
-    document.querySelector('.NavBar a[href="#module1"]').addEventListener('click', function () {
-        HeroDiv.innerHTML = module1Content;
+function initState() {
+    // Clear the current content of the navbar row
+    const navbarRow = document.getElementById('navbarRow');
+    navbarRow.innerHTML = '';
+
+    // Add the selected option and initial choice to the navbar row
+    const selectedOptionElement = document.createElement('div');
+    selectedOptionElement.classList.add('col');
+    navbarRow.appendChild(selectedOptionElement);
+
+    if (initialChoice) {
+        const initialChoiceElement = document.createElement('div');
+        initialChoiceElement.innerHTML = `<button onclick="showSubMenu('${initialChoice}')" class="btn btn-warning">${initialChoice}</button>`;
+        initialChoiceElement.classList.add('col');
+        navbarRow.appendChild(initialChoiceElement);
+    }
+
+    // Create a new set of sub-options
+    const subOptions = generateSubOptions(currentState);
+
+    // Add the new set of sub-option buttons to the navbar row
+    subOptions.forEach((subOption, index) => {
+        const subOptionElement = document.createElement('div');
+        subOptionElement.innerHTML = `<button onclick="showSubMenu('${subOption}')" class="btn btn-secondary">${subOption}</button>`;
+        subOptionElement.classList.add('col');
+        navbarRow.appendChild(subOptionElement);
     });
-    
-    document.querySelector('.NavBar a[href="#module2"]').addEventListener('click', function () {
-        HeroDiv.innerHTML = module2Content;
-    });
+}
 
-    document.querySelector('.NavBar a[href="#listContent"]').addEventListener('click', function () {
-        NavBar.innerHTML = listContent;
-    });
-
-
-    showModule('module1'); // Show the initial module
-    
-    // document.querySelector('.NavBar a[href="#module1"]').addEventListener('click', function (event) {
-    //     event.preventDefault();
-    //     showModule('module1');
-    // });
-    
-    // document.querySelector('.NavBar a[href="#module2"]').addEventListener('click', function (event) {
-    //     event.preventDefault();
-    //     showModule('module2');
-    // });
-    
-    // history.replaceState({}, document.title, window.location.pathname);
-});
 
 function showSubMenu(selectedOption) {
-    // Réinitialiser le contenu du sidebar
-    document.getElementById('sidebar').innerHTML = '';
- 
-    // Mapping des sous-options pour chaque option
-    const subOptionsMap = {
-       'display': ['SubOption 1A', 'SubOption 1B', 'SubOption 1C'],
-       'game': ['SubOption 2A', 'SubOption 2B', 'SubOption 2C'],
-       'option': ['SubOption 3A', 'SubOption 3B', 'SubOption 3C'],
-    };
- 
-    // Ajouter l'option sélectionnée au sidebar
-    const selectedOptionElement = document.createElement('div');
-    selectedOptionElement.innerText = selectedOption;
-    selectedOptionElement.classList.add('selected-option');
-    document.getElementById('sidebar').appendChild(selectedOptionElement);
- 
-    // Ajouter les sous-options au sidebar
-    const subOptions = subOptionsMap[selectedOption] || [];
-    subOptions.forEach(subOption => {
-       const subOptionElement = document.createElement('div');
-       subOptionElement.innerText = subOption;
-       document.getElementById('sidebar').appendChild(subOptionElement);
-    });
- }
- 
+        // Check if the selected option is to the left of the current state
+        const selectedOptionIndex = ['display', 'game', 'option'].indexOf(selectedOption);
+        const currentStateIndex = ['display', 'game', 'option'].indexOf(currentState);
 
-function showModule(moduleId) {
-    // Hide all modules
-    var modules = document.querySelectorAll('.HeroDiv');
-    modules.forEach(function (module) {
-        module.classList.remove('active');
-        history.replaceState({}, document.title, window.location.pathname);
-    });
-
-    // Show the selected module
-    document.getElementById(moduleId).classList.add('active');
+        if (selectedOption === 'home') {
+            // Handle the 'Home' option
+            if (currentState !== 'home') {
+                currentState = 'home';
+                initialChoice = null;
+                initState();  // Call initState to update the navbar row
+            }
+        } else if (selectedOptionIndex < currentStateIndex) {
+            // Reset to the initial state
+            currentState = selectedOption;
+            initialChoice = null;
+            initState();  // Call initState to update the navbar row
+        } else {
+            
+            // Handle sub-options behavior
+            // handleSubOption(selectedOption);
+            currentState = selectedOption;
+            initialChoice = selectedOption;
+            initState();  // Call initState to update the navbar row
+        }
 }
+
+function handleSubOption(subOption) {
+    // Customize this function based on the desired behavior for sub-options
+    switch (subOption) {
+        case 'Display1':
+            // Load content into the heroDiv for Display1
+            loadContent('Logo/logo.html');
+            break;
+        // Add more cases for other sub-options if needed
+        case 'Display2':
+            // Load content into the heroDiv for Display1
+            loadContent('Logo/logo2.html');
+            break;
+        default:
+            break;
+    }
+}
+
+function loadContent(contentFile) {
+    // Load content into the heroDiv based on the specified content file
+    const heroDiv = document.getElementById('heroDiv');
+    fetch(contentFile)
+        .then(response => response.text())
+        .then(data => {
+            heroDiv.innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+
+function renderInitNavbar() {
+    // Customize this function to render another navbar to the init value
+    const navbarRow = document.getElementById('navbarRow');
+    navbarRow.innerHTML = '';
+
+    // Add the selected option and initial choice to the navbar row
+    const selectedOptionElement = document.createElement('div');
+    selectedOptionElement.classList.add('col');
+    navbarRow.appendChild(selectedOptionElement);
+
+    if (initialChoice) {
+        const initialChoiceElement = document.createElement('div');
+        initialChoiceElement.innerHTML = `<button onclick="showSubMenu('${initialChoice}')" class="btn btn-warning">${initialChoice}</button>`;
+        initialChoiceElement.classList.add('col');
+        navbarRow.appendChild(initialChoiceElement);
+    }
+
+    // Create a new set of sub-options
+    const subOptions = generateSubOptions(currentState);
+
+    // Add the new set of sub-option buttons to the navbar row
+    subOptions.forEach((subOption, index) => {
+        const subOptionElement = document.createElement('div');
+        subOptionElement.innerHTML = `<button onclick="showSubMenu('${subOption}')" class="btn btn-secondary">${subOption}</button>`;
+        subOptionElement.classList.add('col');
+        navbarRow.appendChild(subOptionElement);
+    });
+}
+
+
+function generateSubOptions(selectedOption) {
+    // You can customize this function to generate sub-options based on the selected option
+    switch (selectedOption) {
+        case 'home':
+            return ['Display', 'Game', 'Option'];
+        case 'display':
+            return ['Home','Display1', 'Display2', 'Display3'];
+        case 'game':
+            return ['Home','Game1', 'Game2', 'Game3'];
+        case 'option':
+            return ['Home','Option1', 'Option2', 'Option3'];
+        default:
+            return [];
+    }
+}
+
