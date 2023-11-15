513dfd044248ce68ebf0e6fb0f003a064dfefe65
alvachon <alvachon@student.42quebec.com>
2023-11-15 15:45:30 +0000

    Cleaning : This work, might not touch the way it's written right now ...

--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -3,45 +3,42 @@ from django.http import HttpResponseRedirect, HttpResponse
 from core.settings import ENV_FILE
 from django.shortcuts import render
 
-
+#http://127.0.0.1:3000/index/
+# Warning : This URL is extra-step,
 def login_view(request):
     return render(request, 'Index/index.html')
 
+#http://127.0.0.1:3000/
 def home_view(request):
     authorization_code = request.GET.get('code', None)
     if (authorization_code):
         token = get_access_token(authorization_code)
         token_code = requests.post(token)
-        
         access_token = token_code.json()['access_token']
-        
         headers = {'Authorization': 'Bearer ' + access_token}
         url = get_data()
         user_data = requests.get(url, headers=headers)
         return (HttpResponse(user_data.text))
     return render(request, 'Home/home.html')
 
+#http://127.0.0.1:3000/login
+# Warning : Going to this URL will trigger the oauth2 process
 def api_view(request):
-#   Making the url for the api call
     api_url =   ENV_FILE['HTTP_PROTOCOL'] + \
                 ENV_FILE['APP42_DOMAIN'] + \
                 ENV_FILE['APP42_AUTH'] + \
                 '?client_id=' + ENV_FILE['APP42_UID'] + \
                 '&redirect_uri=' + ENV_FILE['APP42_OAUTH_REDIRECT'] + \
                 '&response_type=code'
-                
-#   Redirecting to the api call, put the callback url in a variable
     return (HttpResponseRedirect(api_url))
 
 def get_access_token(autorization_code):
-    
     url   = ENV_FILE['HTTP_PROTOCOL'] + \
             ENV_FILE['APP42_DOMAIN'] + \
             ENV_FILE['APP42_TOKEN']
                 
     client_id     = ENV_FILE['APP42_UID']
     client_secret = ENV_FILE['APP42_SECRET']
-    
     data =  '?grant_type=authorization_code' + \
             '&client_id=' + client_id + \
             '&client_secret=' + client_secret + \
