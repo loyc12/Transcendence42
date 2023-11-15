7c508112bb518c2025fe2a4d5781a05039d5193e
alvachon <alvachon@student.42quebec.com>
2023-11-15 18:40:19 +0000

    Added a active trigger

--- webpage/users/urls.py
+++ webpage/users/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 # The `urlpatterns` list routes URLs to views.
 urlpatterns = [
-    path("", views.user_main),
-    path("create/", views.user_create),
-    path("get/", views.user_get_jimmy),
     path("delete/", views.user_delete_jimmy)
 ]
--- webpage/users/views.py
+++ webpage/users/views.py
@@ -9,37 +9,17 @@ def import_data(user_data):
     
     login = user_data.json()['login']
     if (User.objects.filter(login=login).exists()):
-        return (f"User {login} already exists in database.")
-
-    user =  User.objects.create_user(
-            login=login,
-            display_name=user_data.json()['displayname'],
-            img_link=user_data.json()['image']['link'],
-    )
-    return (login)
-
-def user_main(request):
-    print(request)
-    return (HttpResponse("Reached User endpoint"))
-
-def user_create(request):
-    print(request)
-    print("Try create user :")
-    user = User.objects.create_user(
-        login="alvachon",
-        display_name="Alexandra Vachon",
-        img_link="https://cdn.intra.42.fr/users/alvachon.jpg",
-        password="password",
-    )
-
-    return (HttpResponse(f"User {user.login} created Successfully."))
-
-def user_get_jimmy(request):
-    print(request)
-    user = User.objects.get(login="alvachon")
-
-    print("Got this user from database : ", user)
-    return (HttpResponse(f"User successfully retrieved from database. user : {user}"))
+        User.objects.filter(login=login).update(
+            is_active=1,
+        )
+    else:
+        user =  User.objects.create_user(
+                login=login,
+                display_name=user_data.json()['displayname'],
+                img_link=user_data.json()['image']['link'],    
+                is_active=1,    
+        )
+    return (f"Welcome {login} !")
 
 def user_delete_jimmy(request):
     print(request)
