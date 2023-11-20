9eff407ebb7941c55f172381d5175e480b4bda14
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-11-20 01:02:11 +0000

    HeroDiv logo img

--- webpage/Home/urls.py
+++ webpage/Home/urls.py
@@ -1,11 +1,12 @@
 """ This file contains the URL patterns for the Home app. """
 from django.urls import path
-from .views import home_view#, logo_view, selector_view
+from .views import home_view, logo_view, profile_view
 #from login.views import login_view
 
 urlpatterns = [
     # URL for the main page (home)
     path("", home_view, name='home' ),
-    #path('Logo/', logo_view, name='logo' ),
+    path('logo/', logo_view, name='logo'),
+    path('Profile/', profile_view, name='profile'),
     #path('UserSelect/', selector_view, name='selector' )
 ]
--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -1,12 +1,13 @@
 import requests
 from core.settings import ENV_FILE
 from django.shortcuts import render
-
-from users.views import import_data
 from login.views import get_access_token, get_api_data
+from users.views import import_data
+from .models import UserProfile
 
 #http://127.0.0.1:3000/
 def home_view(request):
+    """ This function is used to render the home page. """
     authorization_code = request.GET.get('code', None)
     if (authorization_code):
         token = get_access_token(authorization_code)
@@ -18,3 +19,15 @@ def home_view(request):
         import_data(user_data, request)
         return render(request, 'Home/home.html')
     return render(request, 'Home/home.html')
+
+def logo_view(request):
+    """ This function is used to render the logo page. """
+    return render(request, 'logo.html')
+
+
+def profile_view(request):
+    """ This function is used to render the profile page. """
+    # Fetching the first user profile for demonstration purposes
+    user_profile = UserProfile.objects.first()
+    return render(request, 'profile.html', {'user_profile': user_profile})
+
