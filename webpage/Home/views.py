cdd7df8e95a33eeabab83c773d7520900cdfe3ff
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-11-20 03:31:40 +0000

    improbable

--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -1,6 +1,7 @@
+""" This file is used to render the home page. """
 import requests
-from core.settings import ENV_FILE
-from django.shortcuts import render, redirect
+# from core.settings import ENV_FILE
+from django.shortcuts import render
 from login.views import get_access_token, get_api_data
 from users.views import import_data
 from .forms import ProfileForm
@@ -12,11 +13,11 @@ def home_view(request):
     authorization_code = request.GET.get('code', None)
     if (authorization_code):
         token = get_access_token(authorization_code)
-        token_code = requests.post(token)
+        token_code = requests.post(token, timeout=10)
         access_token = token_code.json()['access_token']
         headers = {'Authorization': 'Bearer ' + access_token}
         url = get_api_data()
-        user_data = requests.get(url, headers=headers)
+        user_data = requests.get(url, headers=headers, timeout=10)
         import_data(user_data, request)
         return render(request, 'Home/home.html')
     return render(request, 'Home/home.html')
@@ -39,4 +40,3 @@ def profile_view(request):
         form = ProfileForm()
 
     return render(request, 'profile.html', {'form': form})
-
