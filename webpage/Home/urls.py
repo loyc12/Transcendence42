859ca584ed931e118aca1e5c722197cd8a0c52f2
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-11-21 18:35:53 +0000

    game module 3

--- webpage/Home/urls.py
+++ webpage/Home/urls.py
@@ -1,11 +1,12 @@
 """ This file contains the URL patterns for the Home app. """
 from django.urls import path
-from .views import home_view, logo_view, profile_view
+from .views import home_view, logo_view, profile_view, game_view
 #from login.views import login_view
 
 urlpatterns = [
     # URL for the main page (home)
     path("", home_view, name='home' ),
     path('Logo/', logo_view, name='logo' ),
-    path('Profile/', profile_view, name='profile')
+    path('Profile/', profile_view, name='profile'),
+    path('game/', game_view, name='game')
 ]
--- webpage/Home/views.py
+++ webpage/Home/views.py
@@ -30,16 +30,7 @@ def profile_view(request):
     """ This function is used to render the logo page. """
     return render(request, 'profile.html')
 
-def userProfile_view(request):
-    """ This function is used to render the profile page. """
-    # if request.method == 'POST':
-    #     form = ProfileForm(request.POST)
-    #     if form.is_valid():
-    #         # Process the form data (save to the database or perform other actions)
-    #         # For now, just print the data
-    #         print(form.cleaned_data)
-    #         # return redirect('home')
-    # else:
-    #     form = ProfileForm()
 
-    return render(request, 'profile.html', {'form': form})
+def game_view(request):
+    """ This function is used to render the logo page. """
+    return render(request, 'game.html')
