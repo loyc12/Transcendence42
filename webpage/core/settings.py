a85df9271eb5924c775c691cdc709d760e26488d
alvachon <alvachon@student.42quebec.com>
2023-11-22 21:46:48 +0000

    microcomit

--- webpage/core/settings.py
+++ webpage/core/settings.py
@@ -42,7 +42,7 @@ print("DJANGO_DEBUG : ", DJANGO_DEBUG)
 #: keep the secret key used in production secret!
 SECRET_KEY = ENV_FILE["DJANGO_SECRET_KEY"]
 DEBUG = True # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.12.2.6']
 
 # APPS - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - - - |
 INSTALLED_APPS = [
