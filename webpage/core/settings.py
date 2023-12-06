6d3839f59485725347d448d142d163265ca6b622
alvachon <alvachon@student.42quebec.com>
2023-12-06 18:09:07 +0000

    remove spaces

--- webpage/core/settings.py
+++ webpage/core/settings.py
@@ -44,13 +44,7 @@ print("DJANGO_DEBUG : ", DJANGO_DEBUG)
 #: keep the secret key used in production secret!
 DEBUG = True # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = ENV_FILE["DJANGO_SECRET_KEY"]
-ALLOWED_HOSTS = ['localhost',
-                 '127.0.0.1',
-                 '10.12.2.10',
-                 '10.12.2.9',
-                 '10.12.2.8',
-                 '10.12.2.7',
-                 '10.12.2.6']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.12.2.10', '10.12.2.9', '10.12.2.8', '10.12.2.7', '10.12.2.6']
 
 
 # APPS - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - - - |
@@ -109,13 +103,8 @@ SECURE_CONTENT_TYPE_NOSNIFF = True #Againts MIME sniffing
 # X-Frame-Options (Against Clickjacking)
 X_FRAME_OPTIONS = 'DENY' 
 # Secure session management
-<<<<<<< HEAD
 #CSRF_COOKIE_SECURE = True
 #SESSION_COOKIE_SECURE = True # Should be True
-=======
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True # Should be True
->>>>>>> DEV
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_HTTPONLY = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
