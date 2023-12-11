2f7d078befab4ff48b9222e665823275636b889f
GHebert42 <88173405+GHebert42@users.noreply.github.com>
2023-12-11 17:00:37 +0000

    update

--- webpage/Home/static/js/canvasRender.js
+++ webpage/Home/static/js/canvasRender.js
@@ -8,32 +8,6 @@
 let setCurrentState = function (initData) {//update) {
     currentGameInfo = initData;
 
-    // updateCanvas = update
-    // console.log('parseUpdateData : ' + update)
-    
-    // const gameId = update.gameID;
-    // const racketPositions = update.racketPos;
-    // const ballPosition = update.ballPos;
-    // const lastPonger = update.lastPonger;
-    // const scores = update.scores;
-
-    // currentGameInfo = {
-    //     'gameType': update.gameType,
-    //     'sizeInfo': {
-    //         'width': 2048, 'height': 1024, 
-    //         'wRatio': 0.00048828125, 'hRatio': 0.0009765625, 
-    //         'sRacket': 160, 'sBall': 20
-    //         // 'sRacket': 160, 'sBall': 20
-    //     },
-    //     'racketCount': 2,
-    //     'teamCount': 2,
-    //     'orientations': ['y', 'y'],
-    //     'update': {
-    //         'racketPos': [682, 1004, 1365, 1004 ],
-    //         'ballPos': [1024, 682],
-    //         'score' : [0, 0],
-    //     }
-    // }
 };
 
 
@@ -50,6 +24,7 @@ let getPlayerColor = function(rank) {
     const index = rank % playerColors.length;
     return playerColors[index];
 }
+
 let getPlayerShadowColor = function(rank) {
     // Use modulo operator to cycle through colors if there are more ranks than colors
     const index = rank % playerColors.length;
@@ -66,35 +41,9 @@ players.forEach(player => {
     // Now you can use playerColor to set the color in your rendering logic
 });
 
-
 let clearCanvas = function (ctx, w, h) {
     ctx.clearRect(0, 0, w, h); // Clear the entire canvas
 }
-
-// class RenderModule {
-//     constructor(initData) {
-//         this.initData = initData;
-//     }
-//     render() {
-//         // Utilisez les données du initData pour effectuer le rendu
-//         console.log('Rendering with Width:', this.initData.sizeInfo.width);
-//         console.log('Rendering with Height:', this.initData.sizeInfo.height);
-//     }
-// }
-
-
-// Utilisation du module de rendu
-
-// if (initStart) {
-//     const renderModule = new RenderModule(initParam);
-//     const initWidth = initParam.sizeInfo.width;
-//     const initHeight = initParam.sizeInfo.height;
-//     // console.log(initParam);
-//     console.log("init_dataValue width :", initParam.sizeInfo.width);
-//     console.log("init_dataValue height:", initParam.sizeInfo.height);
-//     renderModule.render();
-//     initStart = false;
-// }
 
 let renderCanvas = function (ctx, gameInfo) {
     // const { width, height } = gameInfo.sizeInfo;
@@ -171,17 +120,12 @@ let renderRackets = function(ctx, gameInfo, update) {
             h = gameInfo.racketSize;
             // ctx.fillRect(x, y, gameInfo.ballSize, gameInfo.racketSize); // Assuming racketWidth and racketHeight are defined
         }
-        // else {
-        //     // Render the racket at the specified position
-        //     ctx.fillStyle = 'yellow'; // Racket color (customize as needed)
-        //     ctx.fillRect(x, y, gameInfo.ballSize, gameInfo.ballSize); // Assuming racketWidth and racketHeight are defined
-        // }
-        
         ctx.fillStyle = color;
         ctx.shadowBlur = 40;
         ctx.shadowColor = shadow;
         ctx.fillRect(x, y, w, h);
     }
+    
     // let tot = 0;
     // for (let i = 0; i < initParam.racketInitPos.length; i += 3) {
     //     tot += 1;
@@ -211,6 +155,7 @@ let renderRackets = function(ctx, gameInfo, update) {
 /// Should be the only function in the rendering chain to 
 /// access global variables. Every downstream function call should take 
 /// them as arguments.
+
 // let updateCanvas = function (gameInfo) {
 let updateCanvas = function (gameInfo) {
 
@@ -221,164 +166,6 @@ let updateCanvas = function (gameInfo) {
     // Render the canvas with the new data
     renderCanvas(ctx, gameInfo);
 }
-
-
-// class UpdateGame {
-    //     constructor(canvasId, gameData) {
-    //         this.canvas = document.getElementById(canvasId);
-    //         this.ctx = this.canvas.getContext('2d');
-    //         this.gameData = gameData;
-
-    //        // Get initial game data from data attributes console.log
-    //        this.gameData = {
-    //             'sizeInfo': {
-    //                 'width': parseInt(this.canvas.getAttribute('data-width')),
-    //                 'height': parseInt(this.canvas.getAttribute('data-height')),
-    //                 'sRacket': parseInt(this.canvas.getAttribute('data-sRacket')),
-    //                 'sBall': parseInt(this.canvas.getAttribute('data-sBall'))
-    //             },
-    //         // Add more initial data if needed
-    //     };
-
-    //     // Log initial data to console
-    //     console.log('Initial Game Data:', this.gameData);
-
-    //     // Set up initial game state
-    //     this.initState();
-//     }
-
-//     initState() {
-    //         // Clear the canvas
-    //         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    //         // Render game elements based on initial game data
-    //         this.renderRackets();
-    //         this.renderBall();
-    //         // Add more rendering functions based on your game design
-    //     }
-
-//     renderRackets() {
-    //         const racketCount = this.gameData.racketCount;
-    //         const racketInitPos = this.gameData.racketInitPos;
-
-    //         // Render rackets based on the initial positions
-    //         for (let i = 0; i < racketCount; i++) {
-    //             const xPos = racketInitPos[i * 3];
-    //             const yPos = racketInitPos[i * 3 + 1];
-    //             const racketWidth = this.gameData.sizeInfo.sRacket;
-    //             const racketHeight = 10;  // You can adjust the height
-
-    //             this.ctx.fillRect(xPos, yPos, racketWidth, racketHeight);
-    //         }
-    //     }
-
-//        renderBall() {
-    //         const ballInitPos = this.gameData.ballInitPos;
-    //         const ballSize = this.gameData.sizeInfo.sBall;
-
-    //         console.log(ballInitPos[0]);
-    //         console.log(ballInitPos[1]);
-
-    //         // Render the ball based on the initial position
-    //         this.ctx.beginPath();
-    //         this.ctx.arc(ballInitPos[0], ballInitPos[1], ballSize, 0, 2 * Math.PI);
-    //         this.ctx.fill();
-    //     }
-
-//     updateGameData(newGameData) {
-    //         // Update game data based on the received data
-    //         Object.assign(this.gameData, newGameData);
-
-    //         // Log updated game data to console
-    //         console.log('Updated Game Data:', this.gameData);
-
-    //         // update game state based on the updated game data
-    //         this.initState();
-//     }
-// }
-
-
-
-
-
-
-// // Create an instance of CanvasRender using the gameState.initData
-// const canvasRender = new CanvasRender('gameCanvas', gameState.initData);
-
-// class UpdateGame {
-//     constructor(canvasId, gameData) {
-//         this.canvas = document.getElementById(canvasId);
-//         this.ctx = this.canvas.getContext('2d');
-//         this.gameData = gameData;
-
-//        // Get initial game data from data attributes console.log
-//        this.gameData = {
-//             'sizeInfo': {
-//                 'width': parseInt(this.canvas.getAttribute('data-width')),
-//                 'height': parseInt(this.canvas.getAttribute('data-height')),
-//                 'sRacket': parseInt(this.canvas.getAttribute('data-sRacket')),
-//                 'sBall': parseInt(this.canvas.getAttribute('data-sBall'))
-//             },
-//         // Add more initial data if needed
-//     };
-
-//     // Log initial data to console
-//     console.log('Initial Game Data:', this.gameData);
-
-//     // Set up initial game state
-//     this.initState();
-//     }
-
-//     initState() {
-//         // Clear the canvas
-//         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-//         // Render game elements based on initial game data
-//         this.renderRackets();
-//         this.renderBall();
-//         // Add more rendering functions based on your game design
-//     }
-
-//     renderRackets() {
-//         const racketCount = this.gameData.racketCount;
-//         const racketInitPos = this.gameData.racketInitPos;
-
-//         // Render rackets based on the initial positions
-//         for (let i = 0; i < racketCount; i++) {
-//             const xPos = racketInitPos[i * 3];
-//             const yPos = racketInitPos[i * 3 + 1];
-//             const racketWidth = this.gameData.sizeInfo.sRacket;
-//             const racketHeight = 10;  // You can adjust the height
-
-//             this.ctx.fillRect(xPos, yPos, racketWidth, racketHeight);
-//         }
-//     }
-
-//     renderBall() {
-//         const ballInitPos = this.gameData.ballInitPos;
-//         const ballSize = this.gameData.sizeInfo.sBall;
-
-//         console.log(ballInitPos[0]);
-//         console.log(ballInitPos[1]);
-
-//         // Render the ball based on the initial position
-//         this.ctx.beginPath();
-//         this.ctx.arc(ballInitPos[0], ballInitPos[1], ballSize, 0, 2 * Math.PI);
-//         this.ctx.fill();
-//     }
-
-//     updateGameData(newGameData) {
-//         // Update game data based on the received data
-//         Object.assign(this.gameData, newGameData);
-
-//         // Log updated game data to console
-//         console.log('Updated Game Data:', this.gameData);
-
-//         // update game state based on the updated game data
-//         this.initState();
-//     }
-// }
-
 
 
 //
