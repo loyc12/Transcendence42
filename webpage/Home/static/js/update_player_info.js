7b389ee5dc5aaf614cf50591ede056d9083aad04
alvachon <alvachon@student.42quebec.com>
2023-12-13 18:26:09 +0000

    multi log ok

--- webpage/Home/static/js/update_player_info.js
+++ webpage/Home/static/js/update_player_info.js
@@ -21,6 +21,27 @@ let update_player_info = function (player_info_list) {
 
   console.log('update_player_info CALLED : ' + player_info_list);
 
+
+  let i = 0;
+  for (ply of player_info_list) {
+    imgElemID = `imgPlayer${++i}`;
+    nameElemID = `namePlayer${i}`;
+
+    login = ply.login;
+    img = ply.img;
+    ready = ply.ready;
+
+    console.log('login : ' + login)
+    console.log('img : ' + img)
+    console.log('ready : ' + ready)
+
+    //img_elem = document.getElementById(imgElemID);
+    // document.getElementById(imgElemID) = login;
+    document.getElementById(imgElemID).src = img;
+    document.getElementById(nameElemID).innerHTML = `Player ${i} : ${login}`;
+    // img_elem.src = img;
+    // name_elem.innerHTML = `Player ${i} : ${login}`;
+  }
   // for (ply of players) {
   //   imgElemID = ``
   // }
@@ -28,20 +49,20 @@ let update_player_info = function (player_info_list) {
   // profil_elem = document.getElementById("lobbyProfile1");
   // img_elem1 = document.getElementById("imgPlayer1");
   // img_elem2 = document.getElementById("imgPlayer2");
-  // img_elem3 = document.getElementById("imgPlayer3");
-  img_elem4 = document.getElementById("imgPlayer4");
-  // img_elem1.src = '';
-  // img_elem2.src =
-  // img_elem3.src =
-  // img_elem4.src =
+  // // img_elem3 = document.getElementById("imgPlayer3");
+  // img_elem4 = document.getElementById("imgPlayer4");
+  // // img_elem1.src = '';
+  // // img_elem2.src =
+  // // img_elem3.src =
+  // // img_elem4.src =
 
-  // name_elem1 = document.getElementById("namePlayer1");
-  // name_elem2 = document.getElementById("namePlayer2")
-  // name_elem3 = document.getElementById("namePlayer3")
-  name_elem4 = document.getElementById("namePlayer4")
-  player_name = 'Bobby !';
-  name_elem4.innerHTML = `Player4 : ${player_name}`;////'<div class="align-right4 style="color: #f0ffff; font-size: 40px;" id="namePlayer1">Player1 : Bobby ! </div>';
-  //name_eme2.innerHTML= "Player1 :" + {{ namePlayer1 }}
+  // // name_elem1 = document.getElementById("namePlayer1");
+  // // name_elem2 = document.getElementById("namePlayer2")
+  // // name_elem3 = document.getElementById("namePlayer3")
+  // name_elem4 = document.getElementById("namePlayer4")
+  // player_name = 'Bobby !';
+  // name_elem4.innerHTML = `Player4 : ${player_name}`;////'<div class="align-right4 style="color: #f0ffff; font-size: 40px;" id="namePlayer1">Player1 : Bobby ! </div>';
+  // //name_eme2.innerHTML= "Player1 :" + {{ namePlayer1 }}
 
 
 
