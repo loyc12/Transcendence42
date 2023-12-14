d29cba461980153f80269abbc477dc5efbdeb6f6
alvachon <alvachon@student.42quebec.com>
2023-12-14 20:24:05 +0000

    local 1-1 lobby ok

--- webpage/Home/static/js/game.js
+++ webpage/Home/static/js/game.js
@@ -72,6 +72,7 @@ let request_join_game = async function (gameType) {
   
   //console.log('request_join_game temporarly deactivated. Come back again later.')
   //return ;
+  currentGameType = gameType
   if (gameType === 'Local_1p') {
     payload = _build_join_request_payload('Local_1p', 'Pong', true);
   } else if (gameType === 'Local_2p') {
